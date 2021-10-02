9b346b3e88853bfa317523258e36f4d555301646
moudoder <39887984+moudoder@users.noreply.github.com>
2021-10-02 15:01:11 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -4,8 +4,25 @@ $(document).ready(function () {
     event.preventDefault();
     let id  = $(this).attr('href'),
     top = $(id).offset().top;
+    $('.mobile-menu').removeClass('mobile-menu-active');
     $('body,html').animate({scrollTop: top}, 1500);
   });
   $(".phone").mask("+7 (999) 999-9999");
   
+
+  function windowSize(){
+      if ($(window).width() <= '1200'){
+          $('.services-block').unwrap();
+      } else {
+          
+      }
+  }
+  windowSize()
+
+  $('.mobile-menu-button').on('click', function() {
+  	$('.mobile-menu').addClass('mobile-menu-active');
+  })
+  $('.mobile-menu-close').on('click', function() {
+  	$('.mobile-menu').removeClass('mobile-menu-active');
+  })
 })
